4dd2587992eb86be12d0ca3ef48ca8fc0b4807ab
Gabriel Pozzan da Silva <gabriel.pozzan@unifebe.edu.br>
2026-06-01 17:03:50 -0300

    commit 2

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+let clicker = document.getElementsByClassName(clicker-button)
+
+const upgrade_itens = [
+    { img: "", title: "titulo 1", text: "texto 1" },
+    { img: "", title: "titulo 2", text: "texto 2" },
+    { img: "", title: "titulo 3", text: "texto 3" },
+    { img: "", title: "titulo 4", text: "texto 4" },
+    { img: "", title: "titulo 5", text: "texto 5" },
+    { img: "", title: "titulo 6", text: "texto 6" },
+    { img: "", title: "titulo 7", text: "texto 7" },
+  ];
+  
+
+const list = document.getElementById("list");
+
 const upgradeButton = document.querySelector(".upgrades-button");
 const upgrade = document.querySelector(".upgrades");
+
+list.innerHTML = upgrade_itens.map(upgrade_itens => `
+        <li class="upgrade-item" role="button">
+        <img src="${upgrade_itens.img}" alt="">
+        <h3>${upgrade_itens.title}</h3>
+        <p>${upgrade_itens.text}</p>
+        </li>
+  `).join("");
 
 upgradeButton.addEventListener("click", () => {
     upgrade.classList.toggle("open");
