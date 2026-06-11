6c1106b966a85bb872c0b328677392aff9adf372
Pozzan <gabriel.pozzan@unifebe@edu.br>
2026-06-11 17:20:05 -0300

    developing

--- js/script.js
+++ js/script.js
@@ -5,8 +5,9 @@ function saveGame() {
     localStorage.setItem('autoClickPower', autoClickPower);
     localStorage.setItem('upgradesAcquired', upgradesAcquired);
     localStorage.setItem("upgradeLevels", JSON.stringify(
-        upgrade_itens.map(u => u.level)
-    ));
+        upgrade_itens.map(u => u.level)));
+    localStorage.setItem("criticalChance", criticalChance);
+    localStorage.setItem("criticalMultiplier", criticalMultiplier);
 }
 
 //darkmode verification at the beggining
@@ -19,30 +20,79 @@ if (localStorage.getItem('dark_mode') === 'true') {
     };
 }
 
-// Click function
-show_points.textContent = click;
-
-document.getElementById("clicker-button").addEventListener('click', function() {
-    click += clickPower;
+//ShowContent function
+function updateUI() {
     show_points.textContent = click;
-    
-    saveGame();
+    baseValue.textContent = clickPower;
+    autoClickText.textContent = autoClickPower;
+    upgradesAcquiredText.textContent = upgradesAcquired;
+
+    renderUpgrade();
+    saveGame()
+}
+
+updateUI();
+
+//click float text function
+function createFloatingText(value, x, y) {
+    const text = document.createElement("div");
+
+    text.classList.add("floating-text");
+    text.textContent = `+${value}`;
+
+    text.style.left = `${x}px`;
+    text.style.top = `${y}px`;
+
+    document.body.appendChild(text);
+
+    setTimeout(() => {
+        text.remove();
+    }, 1000);
+}
+
+// Click function
+clickButton.addEventListener("click", (e) => {
+    let earned = clickPower;
+    let critical = false;
+
+    if (
+        criticalChance > 0 &&
+        Math.random() < criticalChance
+    ) {
+        earned *= criticalMultiplier;
+        critical = true;
+    }
+
+    click += earned;
+
+    createFloatingText(
+        earned,
+        e.pageX,
+        e.pageY,
+        critical
+    );
+
+    updateUI();
 });
+
+//click animation
+clickButton.addEventListener('click', () => {
+    clickButton.classList.add("clicked");
+
+    setTimeout(() => {
+        clickButton.classList.remove("clicked");
+    }, 100)
+})
 
 //timer auto click
 setInterval(() => {
     if (autoClickPower > 0) {
         click += autoClickPower;
-        show_points.textContent = click;
-        saveGame();
+        updateUI();
     }
 }, 1000);
 
-//ShowContent
-show_points.textContent = click;
-baseValue.textContent = clickPower;
-autoClickText.textContent = autoClickPower;
-upgradesAcquiredText.textContent = upgradesAcquired;
+
 
 //darkmode function
 darkmode.addEventListener('click', () => {
@@ -61,7 +111,7 @@ darkmode.addEventListener('click', () => {
 });
 
 //reset function
-reset.addEventListener('click', function() {
+reset.addEventListener('click', () => {
     resetMenu.classList.add("open");
     overlay.classList.add("active");
 });
@@ -74,6 +124,8 @@ resetYes.addEventListener('click', () => {
     localStorage.removeItem("upgradesAcquired");
     localStorage.removeItem("upgradeLevels");
     localStorage.removeItem("upgrades");
+    localStorage.removeItem("criticalChance");
+    localStorage.removeItem("criticalMultiplier");
 
     window.location.reload();
 });
@@ -91,23 +143,33 @@ overlay.addEventListener("click", () => {
 
 //calculate price function
 function getPrice(upgrade) {
-    return Math.floor((upgrade.basePrice ?? 0) * Math.pow(2, upgrade.level ?? 0));
+
+    if (upgrade.maxLevel === 1) {
+        return upgrade.basePrice;
+    }
+
+    return Math.floor((upgrade.basePrice ?? 0) * Math.pow(1.5, upgrade.level ?? 0));
 }
 
 //upgrade list component function
 function renderUpgrade() {  
-    list.innerHTML = upgrade_itens.map(upgrade => {
+    list.innerHTML = upgrade_itens
+    .filter(upgrade => click >= (upgrade.unlock ?? 0))
+    .map(upgrade => {
 
         const isRepeatable =
-            upgrade.maxLevel > 1 || upgrade.maxLevel === Infinity;
+            upgrade.maxLevel > 1 ;
+        
+        const isPurchasable = click >= getPrice(upgrade);
+        const isMaxed = upgrade.level >= upgrade.maxLevel;
 
         return `
-            <li id="${upgrade.id}" class="upgrade-item">
+            <li id="${upgrade.id}" class="upgrade-item ${isMaxed ? 'maxed' : ''} ${isPurchasable && !isMaxed ? 'buy' : ''}">
                 <img class="upgrade-img" src="${upgrade.img}" alt="">
 
                 <h3>
                     ${upgrade.title}
-                    ${isRepeatable ? `Lv. ${upgrade.level + 1}` : ""}
+                    ${isRepeatable ? `Lv. ${upgrade.level}` : ""}
                 </h3>
 
                 <p>${upgrade.text}</p>
@@ -134,11 +196,7 @@ if (saved) {
     });
 } else {
     upgrade_itens.forEach(u => u.level = 0);
-}
-
-//trigger list function
-renderUpgrade();
-
+};
 
 
 //open upgrade function
@@ -159,6 +217,11 @@ list.addEventListener("click", function (e) {
     const upgrade = upgrade_itens.find(u => u.id === item.id);
     if (!upgrade) return;
 
+
+    if (upgrade.level >= upgrade.maxLevel)
+        
+        return;   
+
     const price = getPrice(upgrade);
 
     if (click < price) return;
@@ -167,24 +230,20 @@ list.addEventListener("click", function (e) {
 
     if (upgrade.type === "clickPower") {
         clickPower += upgrade.value;
-        upgradesAcquired++
-        
     }
 
     if (upgrade.type === "autoClick") {
         autoClickPower += upgrade.value;
-        upgradesAcquired++
     }
 
+    if (upgrade.type === "criticalClick") {
+        criticalChance = 0.10;
+        criticalMultiplier = 5;
+    }
+
+    upgradesAcquired++;
     upgrade.level++;
 
-    show_points.textContent = click;
-    baseValue.textContent = clickPower;
-    autoClickText.textContent = autoClickPower;
-    upgradesAcquiredText.textContent = upgradesAcquired;
-
-    renderUpgrade();
     
-
-    saveGame();
+    updateUI();
 });
--- js/variables.js
+++ js/variables.js
@@ -16,8 +16,18 @@ let upgradesAcquired = localStorage.getItem("upgradesAcquired")
     ? parseInt(localStorage.getItem("upgradesAcquired"))
     : 0;
 
+let criticalChance = localStorage.getItem("criticalChance")
+    ? parseFloat(localStorage.getItem("criticalChance"))
+    : 0;
+
+let criticalMultiplier = localStorage.getItem("criticalMultiplier")
+    ? parseInt(localStorage.getItem("criticalMultiplier"))
+    : 1;
+
 
 //DOM
+const clickButton = document.querySelector(".clicker-button");
+
 const show_points = document.getElementById("points");
 
 const baseValue = document.getElementById("base-value");
@@ -26,6 +36,7 @@ const upgradesAcquiredText = document.getElementById("upgrades-amount");
 
 
 const list = document.getElementById("list");
+
 
 const upgradeButton = document.querySelector(".upgrades-button");
 const upgrade = document.querySelector(".upgrades");
@@ -52,13 +63,15 @@ const images = {
 
 
 const upgrade_itens = [
-    { id: "morePoints", img: images.leeks, type: "clickPower", value: 1,  title: "More Clicks", text: "+1 point per click", basePrice: 25,  level: 0, maxLevel: 30},
-    { id: "autoClick", img: images.leeks, type: "autoClick", value: 1, title: "Auto Click", text: "+1 point per second", basePrice: 100,  level: 0, maxLevel: 30  },
-    { id: "u3", img: images.leeks, type: "clickPower", value: 1, title: "titulo 3", text: "texto 3", basePrice: 1,  level: 0, maxLevel: 0   },
-    { id: "u4", img: images.leeks, type: "clickPower", value: 1, title: "titulo 4", text: "texto 4", basePrice: 1,  level: 0, maxLevel: 0   },
-    { id: "u5", img: images.leeks, type: "clickPower", value: 1, title: "titulo 5", text: "texto 5", basePrice: 1,  level: 0, maxLevel: 0   },
-    { id: "u6", img: images.leeks, type: "clickPower", value: 1, title: "titulo 6", text: "texto 6", basePrice: 1,  level: 0, maxLevel: 0   },
-    { id: "u7", img: images.leeks, type: "clickPower", value: 1, title: "titulo 7", text: "texto 7", basePrice: 1,  level: 0, maxLevel: 0   },
+    { id: "morePoints", img: images.leeks, type: "clickPower", value: 1,  title: "More Clicks", text: "+1 point per click", basePrice: 25,  level: 0, maxLevel: 10, unlock: 0},
+    { id: "morePoints2", img: images.leeks, type: "clickPower", value: 5,  title: "More Clicks II", text: "+5 point per click", basePrice: 1000,  level: 0, maxLevel: 10, unlock: 500},
+    { id: "morePoints3", img: images.leeks, type: "clickPower", value: 10,  title: "More Clicks III", text: "+10 point per click", basePrice: 10000,  level: 0, maxLevel: 10, unlock: 5000},
+    { id: "autoClick", img: images.leeks, type: "autoClick", value: 1, title: "Auto Click", text: "+1 point per second", basePrice: 100,  level: 0, maxLevel: 5, unlock: 0},
+    { id: "critical", img: images.leeks, type: "criticalClick", value: 1, title: "Critical Hit", text: "10% chance to deal 5x click", basePrice: 500,  level: 0, maxLevel: 1, unlock: 0},
+    { id: "u4", img: images.leeks, type: "clickPower", value: 1, title: "titulo 4", text: "texto 4", basePrice: 1,  level: 0, maxLevel: 0, unlock: 0   },
+    { id: "u5", img: images.leeks, type: "clickPower", value: 1, title: "titulo 5", text: "texto 5", basePrice: 1,  level: 0, maxLevel: 0, unlock: 0   },
+    { id: "u6", img: images.leeks, type: "clickPower", value: 1, title: "titulo 6", text: "texto 6", basePrice: 1,  level: 0, maxLevel: 0, unlock: 0   },
+    { id: "u7", img: images.leeks, type: "clickPower", value: 1, title: "titulo 7", text: "texto 7", basePrice: 1,  level: 0, maxLevel: 0, unlock: 0   },
   ];
 
-const isRepeatable = upgrade.maxLevel > 1 || upgrade.maxLevel === Infinity;
+
